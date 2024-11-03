84bb663aadb02413e7a3d6bfeaf1116b6876d822
hyeyeon9037 <hyeyeon9037@naver.com>
2024-11-03 20:21:11 +0900

    20241103 project

--- HMG/src/main/java/com/study/dicom/config/ImgConfig.java
+++ HMG/src/main/java/com/study/dicom/config/ImgConfig.java
@@ -9,7 +9,7 @@ public class ImgConfig implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/mhg/**")
-		.addResourceLocations("file:///C:/mhg/");
+		.addResourceLocations("file:///Users/hyeye_on/Desktop/file001/mhg/");
 		
 	}
 }
